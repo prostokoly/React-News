34747f00f86d7401f88170e0f431ee533410c579
prostokoly <kolasosnov177@gmail.com>
2026-04-03 00:36:37 +0400

    рефакторинг

--- src/component/Categories.jsx/Categories.jsx
+++ src/component/Categories.jsx/Categories.jsx
@@ -3,6 +3,13 @@ import styles from "./categories.module.css";
 const Categories = ({ categories, setSelectedCategory, selectedCategory }) => {
     return (
         <div className={styles.categories}>
+            <button
+                className={!selectedCategory ? styles.active : styles.item}
+                onClick={() => setSelectedCategory(null)}
+            >
+                All
+            </button>
+
             {categories.map((category) => {
                 return (
                     <button
--- src/component/NewsBanner/NewsBanner.jsx
+++ src/component/NewsBanner/NewsBanner.jsx
@@ -1,17 +1,20 @@
 import Image from "../Image/Image";
 import styles from "./newsBanner.module.css";
 import { formatTimeAgo } from "../../helpers/formatTimeAgo";
+import withSkeleton from "../../helpers/hocs/withSkeleton";
 
 const NewsBanner = ({ item }) => {
     return (
         <div className={styles.banner}>
             <Image image={item?.image} />
-            <h3 className={styles.title}>{item.title}</h3>
+            <h3 className={styles.title}>{item?.title}</h3>
             <p className={styles.extra}>
-                {formatTimeAgo(item.published)} by {item.author}
+                {formatTimeAgo(item?.published)} by {item?.author}
             </p>
         </div>
     );
 };
 
-export default NewsBanner;
+const NewsBannerWithSkeleton = withSkeleton(NewsBanner, "banner", 1);
+
+export default NewsBannerWithSkeleton;
